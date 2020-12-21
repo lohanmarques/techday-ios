f2fdd3a81aafe562a51519d02345c1a8900a28f5
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2020-12-21 16:44:59 -0300

    Evolve PlayerViewController

--- techday-tvOS/PlayerViewController.swift
+++ techday-tvOS/PlayerViewController.swift
@@ -2,13 +2,27 @@
 //  PlayerViewController.swift
 //  techday-tvOS
 //
-//  Created by amanda.rodrigues on 21/12/20.
+//  Created by Vanderlei Martinelli on 21/12/20.
 //
 
 import UIKit
 import AVKit
 
 class PlayerViewController: AVPlayerViewController {
-    
+    var videoURL: URL? {
+        didSet {
+            guard let videoURL = self.videoURL else {
+                return
+            }
 
+            self.player = AVPlayer(url: videoURL)
+            self.player?.play()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
 }
--- techday-tvOS/ViewController.swift
+++ techday-tvOS/ViewController.swift
@@ -7,17 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+final class ViewController: UIViewController {
     lazy var viewModel: ViewModel = ViewModel()
+    weak var playerViewController: PlayerViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        viewModel.delegate = self
+        viewModel.delegate = self
         viewModel.fetchMatches()
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "EmbedPlayerViewController":
+            self.playerViewController = segue.destination as? PlayerViewController
 
+        default:
+            break
+        }
+    }
 }
 
+extension ViewController: MatchesDelegates {
+    func matchesDidChange() {
+        print(self.viewModel.matches)
+    }
+
+    func selectedMatchDidChange() {
+        guard let match = self.viewModel.selectedMatch else {
+            return
+        }
+
+        guard let videoURL = Bundle.main.url(forResource: "\(match.home)-\(match.away)", withExtension: "mp4") else {
+            return
+        }
+
+        self.playerViewController?.videoURL = videoURL
+    }
+}
