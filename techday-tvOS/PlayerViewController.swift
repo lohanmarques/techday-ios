d524614def739a9b9e62e788a28c8255b47e9b7c
Amanda Coutrin <amandacoutrinc@gmail.com>
2020-12-21 16:07:04 -0300

    Adicionando PlayerViewController

---
+++ techday-tvOS/PlayerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerViewController.swift
+//  techday-tvOS
+//
+//  Created by amanda.rodrigues on 21/12/20.
+//
+
+import UIKit
+import AVKit
+
+class PlayerViewController: AVPlayerViewController {
+    
+
+}
