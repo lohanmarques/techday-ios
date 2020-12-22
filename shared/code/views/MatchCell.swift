38f8011043bc9a142bb692444d3df707c47bed84
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2020-12-22 13:01:45 -0300

    Reorganize folders and files

---
+++ shared/code/views/MatchCell.swift
@@ -0,0 +1,71 @@
+//
+//  MatchCell.swift
+//  techday
+//
+//  Created by Lohan Marques on 21/12/20.
+//
+
+import UIKit
+
+let imageCache = NSCache<NSString, UIImage>()
+
+final class MatchCell: UICollectionViewCell {
+
+    let gradient: [UIColor] = [
+        UIColor(red: 0, green: 0, blue: 0, alpha: 0.00),
+        UIColor(red: 0.18, green: 0.56, blue: 0.24, alpha: 1.00)
+    ]
+
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var homeScore: UILabel!
+    @IBOutlet weak var awayScore: UILabel!
+    @IBOutlet weak var tournament: UILabel!
+    @IBOutlet weak var homeLogo: UIImageView!
+    @IBOutlet weak var homeTeam: UILabel!
+    @IBOutlet weak var awayLogo: UIImageView!
+    @IBOutlet weak var awayTeam: UILabel!
+
+    func configure(_ match: Match) {
+        let goals = getGoals(match.score)
+
+        homeScore?.text = goals.first
+        awayScore?.text = goals.last
+        tournament?.text = match.tournament.uppercased()
+        homeTeam?.text = match.home.uppercased()
+        awayTeam?.text = match.away.uppercased()
+        homeLogo?.image = setImage(for: match.home)
+        awayLogo?.image = setImage(for: match.away)
+    }
+
+    func setupUI() {
+        mainView?.addGradient(with: gradient,
+                             startPoint: CGPoint(x: 1.0, y: 0.0),
+                             endPoint: CGPoint(x: 1.0, y: 1.0))
+    }
+
+    func setEnabled(_ enabled: Bool) {
+        mainView?.layer.borderColor = enabled ? Constants.Colors.border.cgColor : UIColor.clear.cgColor
+    }
+
+    private func getGoals(_ score: String) -> [String]  {
+        return score.components(separatedBy: " x ")
+    }
+
+    private func setImage(for name: String) -> UIImage? {
+        if let image = imageCache.object(forKey: name as NSString) {
+            return image
+        }
+
+        guard let path = Bundle.main.path(forResource: "logo-\(name)", ofType: "png") else {
+            return nil
+        }
+
+        guard let image = UIImage(contentsOfFile: path) else {
+            return nil
+        }
+
+        imageCache.setObject(image, forKey: name as NSString)
+
+        return image
+    }
+}
---
+++ shared/code/views/MatchesViewController.swift
@@ -0,0 +1,109 @@
+//
+//  MatchesViewController.swift
+//  techday-tvOS
+//
+//  Created by amanda.rodrigues on 21/12/20.
+//
+
+import UIKit
+
+class MatchesViewController: UICollectionViewController {
+    private let reuseIdentifier: String = String(describing: MatchCell.self)
+
+    var matches: [Match]? {
+        didSet {
+            reloadData()
+        }
+    }
+    
+    var selectedMatch: Match? {
+        didSet {
+            reloadData()
+        }
+    }
+    
+    weak var viewModel: ViewModel?
+
+    #if os(tvOS)
+    var currentFocusIndexPath: IndexPath?
+    #endif
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView?.register(UINib(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    private func reloadData() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+}
+
+// MARK: DATASOURCE
+extension MatchesViewController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.matches?.count ?? 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        if let match = self.matches?[indexPath.item], let cell = cell as? MatchCell {
+            cell.configure(match)
+            cell.setupUI()
+            cell.setEnabled(match == viewModel?.selectedMatch)
+        }
+    
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let match = matches?[indexPath.row] else { return }
+
+        #if os(iOS)
+        viewModel?.selectMatch(match)
+        #endif
+    }
+
+    #if os(tvOS)
+    override func collectionView(_ collectionView: UICollectionView, canFocusItemAt indexPath: IndexPath) -> Bool {
+        currentFocusIndexPath = indexPath
+        return true
+    }
+
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        guard let indexPath = currentFocusIndexPath else { return }
+        guard let match = matches?[indexPath.row] else { return }
+
+        viewModel?.selectMatch(match)
+    }
+
+    #endif
+}
+
+// MARK: LAYOUT
+extension MatchesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        #if os(tvOS)
+        return CGSize(width: 380, height: 530)
+        #elseif os(iOS)
+        return CGSize(width: 150, height: 180)
+        #endif
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 15, left: 15, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+}
