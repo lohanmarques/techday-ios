d68809362d49ec249a2fee29369ad89ffaa61be9
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2020-12-22 10:26:27 -0300

    Add splash screen

---
+++ techday-tvOS/MatchCell.swift
@@ -0,0 +1,22 @@
+//
+//  MatchCell.swift
+//  techday-tvOS
+//
+//  Created by Vanderlei Martinelli on 22/12/20.
+//
+
+import UIKit
+
+final class MatchCell: UICollectionViewCell {
+    func configure(_ match: Match) {
+
+    }
+
+    func setupUI() {
+
+    }
+
+    func setEnabled(_ isEnabled: Bool) {
+
+    }
+}
